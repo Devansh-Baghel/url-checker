6296174443d27373aaff0875831dacae529830cb
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-12 06:56:17 +0530

    Added a command to clear all data

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ app = typer.Typer()
 data = [];
 
 # Getting the previously stored data from ./data.csv
+# Shows all data
 @app.command("show")
 def getData():
 	with open("data.csv", 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
 			data.append(row)
-			print(row)
+			print(row[0])
 			# show data using rich
 
-# getData()
 
+# Add new website to data
+@app.command("add")
+def addUrl(url):
+	url = [url] # have to do this because csv writer works better with lists
+	with open("data.csv", 'a') as file:
+		writer = csv.writer(file)
+		writer.writerow(url)
+	print("Added " + url[0])
+	# Display confirmation to the user using rich!
+
+
+# Checks all websites
 @app.command("check")
 def getUrlStatus():
 	getData()
@@ -27,14 +39,13 @@ def getUrlStatus():
 		print(url_status)
 
 
-@app.command("add")
-def addUrl(url):
-	url = [url] # have to this because csv writer works better with lists
-	with open("data.csv", 'a') as file:
-		writer = csv.writer(file)
-		writer.writerow(url)
-	print("Added " + url[0])
-	# Display confirmation to the user using rich!
+# Clears all data
+@app.command("clear-all")
+def clearAll():
+	data = [];
+	with open("data.csv", "w") as file:
+		file.write("")
+	print("Cleared all data")
 
 
 if __name__ == "__main__":
